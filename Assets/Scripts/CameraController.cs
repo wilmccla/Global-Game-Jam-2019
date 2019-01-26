23c54897fea26d4e81da5d6673b19bce183b4c23
Alex <thatbuddhadude@gmail.com>
2019-01-26 03:33:27 -0700

    Player Incoming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,41 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
+    public float speed;
 
-    public enum RotationAxis
+    void Start()
     {
-        MouseX = 1,
-        MouseY = 2
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
-
-    public RotationAxis axes = RotationAxis.MouseX;
-
-    public float minimumVert = -45.0f;
-    public float maximumVert = 45.0f;
-
-    public float sensHorizontal = 10.0f;
-    public float sensVertical = 10.0f;
-
-    public float _rotationX = 0;
-
-    // Update is called once per frame
     void Update()
     {
-        if (axes == RotationAxis.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
-        }
-        else if (axes == RotationAxis.MouseY)
-        {
-            _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
-            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //Clamps the vertical angle within the min and max limits (45 degrees)
-
-            float rotationY = transform.localEulerAngles.y;
-
-            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-        }
+        float mouseInput = Input.GetAxis("Mouse Y");
+        Vector3 lookhere = new Vector3(mouseInput * speed * -1, 0, 0);
+        transform.Rotate(lookhere);
     }
 }
