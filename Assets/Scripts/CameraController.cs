e45a9de54c01634772efa06d886b9933ef48ccc2
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-01-26 01:07:12 -0700

    "Fixed" playerController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float speedHorizontal = 2f;
-    public float speedVertical = 2f;
 
-    private float yaw = 0f;
-    private float pitch = 0f;
-
-	// Use this for initialization
-	void Start () {
-
-	    Cursor.visible = false;
-	    Cursor.lockState = CursorLockMode.Locked;
-
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
     }
-	
-	// Update is called once per frame
-	void Update ()
-	{
 
-	    yaw += speedHorizontal * Input.GetAxis("Mouse X");
-	    pitch -= speedVertical * Input.GetAxis("Mouse Y");
+    public RotationAxis axes = RotationAxis.MouseX;
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
 
-	}
+    public float sensHorizontal = 10.0f;
+    public float sensVertical = 10.0f;
+
+    public float _rotationX = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
+        }
+        else if (axes == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //Clamps the vertical angle within the min and max limits (45 degrees)
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
 }
