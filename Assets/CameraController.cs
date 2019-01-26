a7a216dacf27566383b306b6b7b22d4583c84ff6
William McClay <wilmccla@uat.edu>
2019-01-25 23:37:59 -0700

    Camera Controller Script

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float speedHorizontal = 2f;
+    public float speedVertical = 2f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    yaw += speedHorizontal * Input.GetAxis("Mouse X");
+	    pitch -= speedVertical * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+	}
+}
