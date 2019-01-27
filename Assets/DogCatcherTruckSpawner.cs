fcc241a201c89ce59e24ae14372ccfa2e924b03c
Alex <thatbuddhadude@gmail.com>
2019-01-27 04:00:38 -0700

    Animations n stuff

---
+++ Assets/DogCatcherTruckSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogCatcherTruckSpawner : MonoBehaviour {
+
+	public GameObject truck;
+	// Use this for initialization
+	void Start () {
+		truck.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player"){
+			truck.SetActive(true);
+		}
+	}
+}
