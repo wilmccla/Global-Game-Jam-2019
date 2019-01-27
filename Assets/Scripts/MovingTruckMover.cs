f984bcb3acad13b1af0a3d5f4150756df8fc1f65
KNOWN-UNIVERSE\jacheima <jacheima@uat.edu>
2019-01-27 12:13:48 -0700

    Traffic is working now

---
+++ Assets/Scripts/MovingTruckMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTruckMover : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = -Vector3.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerController>().Respawn();
+            Destroy(this.gameObject);
+
+        }
+    }
+}
