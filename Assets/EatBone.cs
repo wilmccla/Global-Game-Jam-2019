0621863b95b34db3f3e7381ddc23987f41ab8d5b
KNOWN-UNIVERSE\jacheima <jacheima@uat.edu>
2019-01-27 14:40:46 -0700

    Changed up the AI visuals & put in a dumpster

---
+++ Assets/EatBone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatBone : InteractObject
+{
+
+    public override void Interact(GameObject player)
+    {
+        //player.GetComponent<PlayerStuff>().boneCount++;
+        Destroy(this.gameObject);
+    }
+}
