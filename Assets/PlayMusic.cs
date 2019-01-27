964ea0cef5e823f7c1df79adba650e9c751c124b
kyle <kylecrafty@gmail.com>
2019-01-27 14:35:45 -0700

    Input Makeshift audio and added more textures -Taylor

---
+++ Assets/PlayMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+
+    //public GameObject trigger;
+    public AudioSource audioClip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            audioClip.mute = false;
+
+        }
+    }
+}
