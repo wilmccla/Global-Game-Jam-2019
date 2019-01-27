7dfee5ef567f1991f556dcf4e4bc866ec595f86d
Alex <thatbuddhadude@gmail.com>
2019-01-26 22:30:55 -0700

    Food bowel

--- Assets/Bowl.cs
+++ Assets/Bowl.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Bowl : InteractObject {
 	public bool eaten = false;
+	ParticleSystem particles;
+
+	void Start(){
+		particles = GetComponentInChildren<ParticleSystem>();
+	}
     public override void Interact(GameObject player)
     {
 		if (!eaten){
         	eaten = true;
 			interactText = "Empty";
+			particles.Play();
+						GetComponentInChildren<MeshRenderer>().enabled = false;
+			Invoke("hideFood",1);
 		}
 
     }
+
+		void hideFood(){
+
+			particles.Stop();
+		}
 
 }
