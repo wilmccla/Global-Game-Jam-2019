7d5a7c47c1c55c6d20690c07fbb199b584e7f49a
Alex <thatbuddhadude@gmail.com>
2019-01-26 20:43:05 -0700

    Dog food

---
+++ Assets/Bowl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : InteractObject {
+	public bool eaten = false;
+    public override void Interact(GameObject player)
+    {
+		if (!eaten){
+        	eaten = true;
+			interactText = "Empty";
+		}
+
+    }
+
+}
